147fd3c897780527dd43b884058d8c3f4b14a49d
Tenzin Wangchuk <02230273.cst@rub.edu.bt>
2024-03-15 13:43:55 +0600

    Student info manager

--- Studentinfomanager.py
+++ Studentinfomanager.py
@@ -4,33 +4,25 @@ students_dict = {}
 
 # Add student information
 name = input("Enter student's name: ")
-age = input("Enter student's age: ")
-grade = input("Enter student's grade: ")
+age = int(input("Enter student's age: "))
+grade = int(input("Enter student's grade: "))
 students_list.append(name)
 students_dict[name] = {"age": age, "grade": grade}
 print("Student information added successfully!")
 print(students_dict.items())
 
 # Search for a student by name 
-search_name = input("Enter the name of the student: ")
+search_name = input("Enter the name of the student to search or simply enter to skip: ")
 if search_name in students_list:
-    print(f"student found! Author: {students_dict[search_name]}")
+    info = students_dict[search_name]
+    print(f"Name: {search_name}, Age: {info['age']}, Grade: {info['grade']}")
 else:
     print("student not found!")
 
 # Remove a student
 remove_name = input("Enter the name of the student to remove or simply enter to skip: ")
 if remove_name in students_list:
-    del students_dict(remove_name)
-    students_list.remove(remove_name)
-    print("Student removed sucessfully!")
-else:
-    print("Student not found!")
-
-# Remove a student
-remove_name = input("Enter the name of the student to remove or simply enter to skip: ")
-if remove_name in students_list:
-    del students_dict(remove_name)
+    del students_dict[remove_name]
     students_list.remove(remove_name)
     print("Student removed sucessfully!")
 else:
